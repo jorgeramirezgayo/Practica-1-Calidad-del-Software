84dfc4228104932ad8cea6a8ee29e2737d1fd114
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2024-05-02 16:45:34 +0200

    add feature in gherkhin and ruby definitions for android test

--- app/src/androidTest/calabashTest/features/step_definitions/calabash_steps.rb
+++ app/src/androidTest/calabashTest/features/step_definitions/calabash_steps.rb
@@ -1,1 +1,11 @@
 require 'calabash-android/calabash_steps'
+
+
+When(/^I press "([^"]*)"$/) do |button_name|
+end
+
+Given(/^I see "([^"]*)"$/) do |text|
+end
+
+Then(/^I see "([^"]*)"$/) do |text|
+end
