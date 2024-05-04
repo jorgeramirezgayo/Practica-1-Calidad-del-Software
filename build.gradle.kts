008069ac2b502c0272c32f3493d3595b04b80d0d
jorgeramirezgayo <jorgeramirezgayo@outlook.es>
2024-05-04 15:35:59 +0200

    Unit tests fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-}
-
-android {
-    compileSdkVersion(33)
-    buildToolsVersion("33.0.1")
-
-    defaultConfig {
-        applicationId = "com.example.easynotes"
-        minSdkVersion(21)
-        targetSdkVersion(31)
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
+    testImplementation(kotlin("test"))
+}
 
-    // Aquí van las dependencias de prueba
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test:core-ktx:1.4.0")
-    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
+tasks.test {
+    useJUnitPlatform()
 }
