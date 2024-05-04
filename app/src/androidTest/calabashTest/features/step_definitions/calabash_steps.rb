1d9112f3152fed69ac8e4376a8ad468f978dbee1
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2024-05-04 16:33:00 +0200

    add scenario

--- app/src/androidTest/calabashTest/features/step_definitions/calabash_steps.rb
+++ app/src/androidTest/calabashTest/features/step_definitions/calabash_steps.rb
@@ -1,6 +1,6 @@
 require 'calabash-android/calabash_steps'
 
-
+# Scenario by default
 When(/^I press "([^"]*)"$/) do |button_name|
 end
 
@@ -9,3 +9,18 @@ end
 
 Then(/^I see "([^"]*)"$/) do |text|
 end
+
+# Scenario Dark Mode
+Given(/^the user is in the application settings$/) do
+    touch("Ajustes")
+  end
+  
+  When(/^the user taps on the "(.*?)" option$/) do |option|
+    touch("ModoOscuro")
+  end
+  
+  Then(/^the application's color scheme changes to dark$/) do
+    wait_for_element_exists("* id:'ModoOscuro'")
+    assert_element_exists("* id:'ModoOscuro'")
+  end
+  
