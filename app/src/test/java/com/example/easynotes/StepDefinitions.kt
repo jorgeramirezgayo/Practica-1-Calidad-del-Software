c9a6e7fa27d3c280e2d62cd2f8b2fb6774f781f2
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2024-05-05 16:04:52 +0200

    add third junit test to cucumber tests successfully

--- app/src/test/java/com/example/easynotes/StepDefinitions.kt
+++ app/src/test/java/com/example/easynotes/StepDefinitions.kt
@@ -19,13 +19,16 @@ class StepDefinitions {
     private var relativePath: String = "path"
     private var tags: MutableList<String> = mutableListOf()
     private lateinit var note: NoteModel
-    private lateinit var editedNote: NoteModel
+    private lateinit var note2: NoteModel
+    private lateinit var allNotes: Map<Int,NoteModel>
+//    private lateinit var editedNote: NoteModel
 //    private lateinit var context: Context
 
     @Before
     fun setUp() {
         note = NoteModel(title, color, text, id, relativePath, tags)
-        editedNote = NoteModel("", 0, "", 0, "", tags)
+        note2 = NoteModel("", 0, "", 0, "", tags)
+//        editedNote = NoteModel("", 0, "", 0, "", tags)
 
     }
 
@@ -67,6 +70,30 @@ class StepDefinitions {
         }
     }
 
+
+    @Given("two notes, one with {string}, color {int}, text {string}, id {int}, a relative path {string} and a list of tags and the other one with {string}, color {int}, text {string}, id {int}, a relative path {string} and a list of tags")
+    fun twoNotesWDetails(title1: String, color1: Int,  text1: String, id1:Int, relativePath1:String,title2: String, color2: Int,  text2: String, id2:Int, relativePath2:String) {
+        this.note = NoteModel(title1, color1, text1, id1, relativePath1, this.tags)
+        this.note2 = NoteModel(title2, color2, text2, id2, relativePath2, this.tags)
+        NoteLoader.addNote(this.note)
+        NoteLoader.addNote(this.note2)
+    }
+
+
+    @When("all notes are retrieved with path {string} provided")
+    fun getAllNotes(relativePath:String) {
+        val allNotes = NoteLoader.getNotes("path")
+        this.allNotes = allNotes
+    }
+
+    @Then("the note with id {int} and the note with the id {int} should be 2 in size as well as portrayed")
+    fun notesRetrieved(id1: Int, id2: Int) {
+        assertEquals(2, this.allNotes.size)
+        assertEquals(this.note, this.allNotes[1])
+        assertEquals(this.note2, this.allNotes[2])
+    }
+
+
 //    @Given("an edited note with title {string}, color {int}, text {string}, id {int}, a relative path {string} and an empty list of tags")
 //    fun certainEditedNote(title: String, color: Int,  text: String, id:Int, relativePath:String) {
 //        aNoteWDetails(title, color, text, id, relativePath)
@@ -87,6 +114,8 @@ class StepDefinitions {
 //    fun assertEditedNote(id: Int){
 //        assertEquals(this.editedNote, NoteLoader.getNote(this.id))
 //    }
+
+
 
 
 }
---
+++ app/src/test/java/com/example/easynotes/cucumberTest/features/getAllNodesTest.feature
@@ -0,0 +1,6 @@
+Feature: Get notes
+
+  Scenario: Get all notes
+    Given two notes, one with "Título 1", color 1, text "Contenido 1", id 1, a relative path "path" and a list of tags and the other one with "Título 2", color 2, text "Contenido 2", id 2, a relative path "path" and a list of tags
+    When all notes are retrieved with path "path" provided
+    Then the note with id 1 and the note with the id 2 should be 2 in size as well as portrayed
